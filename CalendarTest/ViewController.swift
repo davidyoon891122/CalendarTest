00b99001d08c25c82a358ee001c65b8fb84c28fa
davidyoon <qqpo12@naver.com>
2021-09-02 22:03:45 +0900

    add self made calender skeleton

--- CalendarTest/ViewController.swift
+++ CalendarTest/ViewController.swift
@@ -9,11 +9,108 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var prevButton:UIButton = {
+        let button = UIButton()
+        button.setTitle("Pre", for: .normal)
+        button.setTitleColor(.red, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.borderWidth = 2
+        button.layer.borderColor = UIColor.lightGray.cgColor
+        return button
+    }()
+    
+    
+    private var displayMonthLabel:UILabel = {
+        let label = UILabel()
+        label.text = "Test"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 15)
+        label.textColor = .red
+        label.layer.borderWidth = 2
+        label.layer.borderColor = UIColor.lightGray.cgColor
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private var nextButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Next", for: .normal)
+        button.setTitleColor(.red, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.borderWidth = 2
+        button.layer.borderColor = UIColor.lightGray.cgColor
+        return button
+    }()
+    
+
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        collectionView.backgroundColor = .blue
+        view.addSubview(prevButton)
+        view.addSubview(displayMonthLabel)
+        view.addSubview(nextButton)
+        view.addSubview(collectionView)
+        setUILayout()
+        
     }
-
-
+    
+    
+    private func initCollection() {
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+        
+    }
+  
+    
+    private func setUILayout() {
+        prevButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+        prevButton.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 0).isActive = true
+        prevButton.widthAnchor.constraint(equalToConstant: 60).isActive = true
+        prevButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        
+        displayMonthLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+        displayMonthLabel.leftAnchor.constraint(equalTo: prevButton.rightAnchor, constant: 0).isActive = true
+        displayMonthLabel.rightAnchor.constraint(equalTo: nextButton.leftAnchor, constant: 0).isActive = true
+        displayMonthLabel.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        
+        nextButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+        nextButton.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: 0).isActive = true
+        nextButton.widthAnchor.constraint(equalToConstant: 60).isActive = true
+        nextButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        
+        collectionView.topAnchor.constraint(equalTo: displayMonthLabel.bottomAnchor, constant: 0).isActive = true
+        collectionView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 0).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0).isActive = true
+        collectionView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: 0).isActive = true
+        
+        
+    }
+    
 }
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return 7
+        default:
+            return 0
+        }
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+    
+}
